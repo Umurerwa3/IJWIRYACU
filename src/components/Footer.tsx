477c7907b5d64b243cc3261b72950fd2f55b3e33
Umurerwa3 <o.umurerwa@alustudent.com>
2025-11-22 17:46:42 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -55,7 +55,7 @@ const Footer: React.FC = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-gray-400" />
-                <span className="text-gray-300">+250 XXX XXX XXX</span>
+                <span className="text-gray-300">+250 782 107 649</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-gray-400" />
@@ -67,7 +67,7 @@ const Footer: React.FC = () => {
 
         <div className="border-t border-gray-800 mt-8 pt-8 flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-400 text-sm">
-            © 2024 IjwiRyacu. All rights reserved.
+            © 2025 IjwiRyacu. All rights reserved.
           </p>
           <div className="flex space-x-6 mt-4 md:mt-0">
             <a href="#" className="text-gray-400 hover:text-primary-400 text-sm transition-colors">Privacy Policy</a>
